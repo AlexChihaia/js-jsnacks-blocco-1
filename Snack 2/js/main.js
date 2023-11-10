7f30abcdcfef9a1a2de25ace2532ec03288bd518
AlexChihaia <alexchihaia84@gmail.com>
2023-11-10 11:26:46 +0100

    feat : altre correzioni

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -8,11 +8,11 @@ const parola2 = prompt("Inserisci la seconda parola:");
 /* Il software stampa prima la parola più corta, poi la parola più lunga */
 
 if (parola1.length > parola2.length) {
-    console.log(parola2, 'Numero di lettere:' parola2.length);
-    console.log(parola1, 'Numero di lettere:' parola1.length);
+    console.log(parola2, 'Numero di lettere:', parola2.length);
+    console.log(parola1, 'Numero di lettere:', parola1.length);
 } else if (word1.length < word2.length) {
-    console.log(parola1, 'Numero di lettere:' parola1.length);
-    console.log(parola2, 'Numero di lettere:' parola2.length);
+    console.log(parola1, 'Numero di lettere:', parola1.length);
+    console.log(parola2, 'Numero di lettere:', parola2.length);
 } else {
     console.log("Le parole hanno la stessa lunghezza.");
 }
