ed7cab4a329136556df8fc20387af2fabeecbe8f
AlexChihaia <alexchihaia84@gmail.com>
2023-11-10 12:00:44 +0100

    feat : snack 3

--- Snack 3/js/main.js
+++ Snack 3/js/main.js
@@ -2,4 +2,23 @@
 
 
 
+/* Il software deve chiedere per 10 volte all’utente di inserire un numero. */
+let somma = 0;
 
+for (let i = 0; i < 10; i++) {
+    const num = parseInt(prompt('Inserisci un numero:'));
+
+    if (isNaN(num)) {
+        alert('Non è un numero , inserisci nuovamente');
+        i--;
+    } else {
+        somma = somma + num;
+    }
+}
+
+
+
+/* Il programma stampa la somma di tutti i numeri inseriti. */
+
+
+console.log('La somma di tutti i numeri inseriti è: ' + somma);
